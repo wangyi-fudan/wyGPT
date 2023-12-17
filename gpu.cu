be2c15b5bf1e453aee2740e02cd6b9545bb76923
王一 WangYi <godspeed_china@yeah.net>
2023-12-17 13:26:38 +0800

    Update gpu.cu

--- gpu.cu
+++ gpu.cu
@@ -228,7 +228,7 @@ int	main(int	ac,	char	**av){
 	}
 	if(!model.load(model_file.c_str())){	fprintf(stderr,"fail to load %s\n",model_file.c_str());	return	0;	}
 	timeval	beg,end;	gettimeofday(&beg,NULL);
-	cout<<model.generate(av[optind],context)<<'\n';
+	cout<<model.generate(av[optind],context)<<'\n';	// the second parameter can be arbitary long
 	gettimeofday(&end,NULL);
 	cerr<<end.tv_sec-beg.tv_sec+1e-6*(end.tv_usec-beg.tv_usec)<<'\n';
 	cublasDestroy(handle);
